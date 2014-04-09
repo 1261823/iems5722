a8b00a9b46333bf0667875fbeebe7afb0efb3fe4
mtleung <m.leung27@gmail.com>
2014-04-09 11:13:39 +0800

    fixed layouts mixing up sender and receiver

--- ChatApp/src/com/iems5722/chatapp/gui/PrivateChatAdapter.java
+++ ChatApp/src/com/iems5722/chatapp/gui/PrivateChatAdapter.java
@@ -4,6 +4,7 @@ import com.iems5722.chatapp.R;
 import com.iems5722.chatapp.database.TblChat;
 import com.iems5722.chatapp.database.TblGlobalChat;
 import com.iems5722.chatapp.database.TblUser;
+import com.iems5722.chatapp.gui.GlobalChatAdapter.ViewHolder;
 import com.iems5722.chatapp.preference.UnitConverter;
 
 import android.content.Context;
@@ -18,6 +19,10 @@ public class PrivateChatAdapter extends SimpleCursorAdapter {
 	public final static String TAG = "PrivateChatAdapter";
 	private Context context;
 	private int layout;
+	private LayoutInflater mInflater;
+	
+	private static final int TYPE_SENT = 0;
+	private static final int TYPE_RECV = 1;	
 	
 	private TextView vMsgId;
 	private TextView vMsgAuthor;
@@ -28,32 +33,76 @@ public class PrivateChatAdapter extends SimpleCursorAdapter {
 		super(context, layout, c, from, to, flags);
 		this.context = context;
 		this.layout = layout;	
+		mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
+	
+	private int getItemViewType(Cursor cursor) {
+		//String[] colname = cursor.getColumnNames();
+		//int i = 0;
+		//int counter = cursor.getCount();
+		//Log.d(TAG, Integer.toString(counter));
+		//for (String s:colname) {
+		//	Log.d(TAG, "col " + s + " val " + cursor.getString(i));
+		//	i++;
+		//}
+		int colAuthor = cursor.getColumnIndex(TblUser.USER_ID);
+		String msgAuthor = cursor.getString(colAuthor);
+		if (msgAuthor.equals(Activity_TabHandler.userId)) {
+			return TYPE_SENT;
+		}
+		else {
+			return TYPE_RECV;
+		}
 	}
 	
 	@Override
+	public int getItemViewType(int position) {
+		Cursor cursor = (Cursor) getItem(position);
+		return getItemViewType(cursor);
+	}
+	
+	@Override
+	public int getViewTypeCount() {
+		return 2;
+	}
+	
+	public static class ViewHolder {
+		public View viewholder;
+	}	
+	
+	@Override
 	public View newView(Context context, Cursor cursor, ViewGroup parent) {
-		//log.d(APP_TAG, "newView");
+		//Log.d(TAG, "newView");
 		Cursor c = getCursor();
-		final LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View v;
-		int msgAuthor = c.getColumnIndex(TblUser.USER_NAME);
-		String dbMsgAuthor = c.getString(msgAuthor);
+		ViewHolder holder = new ViewHolder();
+		View v = null;
+		
+		//change the message display if is own message
+		int colAuthor = c.getColumnIndex(TblUser.USER_ID);
+		String msgAuthor = c.getString(colAuthor);
 
-		if (dbMsgAuthor.equals(Activity_TabHandler.msgUsername)) {
-			//Log.i(TAG, "sent");
-			v = inflater.inflate(R.layout.chat_message_sent, null);
-		}
+		//LayoutParams params;
+		//Log.i(TAG, "Msg " + msg + " from " + msgAuthor + " vs " + Activity_TabHandler.userId);
+		if (msgAuthor.equals(Activity_TabHandler.userId)) {
+			//Log.i(TAG, "Msg from self");
+			v = mInflater.inflate(R.layout.chat_message_sent, parent, false);
+			holder.viewholder = (View) v.findViewById(R.id.chat_message_sent);
+		}		
 		else {
-			//Log.i(TAG, "recv");
-			v = inflater.inflate(layout, null);
+			//Log.i(TAG, "Msg from other");
+			v = mInflater.inflate(R.layout.chat_message_recv, parent, false);
+			holder.viewholder = (View) v.findViewById(R.id.chat_message_recv);
 		}
-		loadView(v, context, c);
+		v.setTag(holder);
+		//v.setLayoutParams(params);
+		//loadView(v, context, c);
 		return v;
 	}
 	
 	@Override
 	public void bindView(View v, Context context, Cursor c) {
 		//log.d(APP_TAG, "bindView");
+		ViewHolder holder = (ViewHolder) v.getTag();
 		loadView(v, context, c);
 	}
 	
